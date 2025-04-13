26d28a4e5031688443a06200b1abb923ef672725
[XY Q] <[qiu15022005@gmail.com]>
2025-04-13 19:01:52 +0200

    尝试python

---
+++ calcul.py
@@ -0,0 +1,16 @@
+a = int(input("Un número: "))
+op = input("Un operador: ")
+b = int(input("Un altre número: "))
+
+if op == "+": 
+    resultat = a+b
+elif op == "-": 
+    resultat = a-b
+elif op == "*": 
+    resultat = a*b
+elif op == "/": 
+    resultat = a/b
+else: 
+    print("No vàlid") 
+    resultat = None
+print(resultat)
